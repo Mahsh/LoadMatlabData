aa7d7dbbde6da1bd0eeec1ba0816ec46014967cd
Mahsh <542246946@qq.com>
2018-09-13 21:53:44 +0800

    add python scripts and data

---
+++ LoadMatData.py
@@ -0,0 +1,12 @@
+import scipy.io as sio
+import numpy as np
+
+if __name__ == "__main__":
+    mat = sio.loadmat('./smallworld256_sourcedata_SI.mat')  # mat is a dictionary
+    print(type(mat))
+    print(mat)
+    print(mat['smallworld256_label_SI_block'].shape)
+    new_data1 = mat['smallworld256_label_SI_block']   # smallworld256_label_SI_block and smallworld256_observedata_SI are two variables store in smallworld256_sourcedata_SI.mat.
+    new_data2 = mat['smallworld256_observedata_SI']
+    print(type(new_data1))
+    print(new_data1)
